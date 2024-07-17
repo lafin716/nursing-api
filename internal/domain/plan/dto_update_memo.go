392666d26a0db7bf3643a1d75e0ede83e988d9b0
lafin716 <lafin716@gmail.com>
2024-07-17 21:45:46 +0900

    fix 메모 빈값 업데이트 지원

--- internal/domain/plan/dto_update_memo.go
+++ internal/domain/plan/dto_update_memo.go
@@ -6,5 +6,5 @@ type UpdateMemoRequest struct {
 	UserId     uuid.UUID
 	TimezoneId uuid.UUID `json:"timezone_id" validate:"required"`
 	Date       string    `json:"date" validate:"required"`
-	Memo       string    `json:"memo" validate:"required"`
+	Memo       string    `json:"memo"`
 }
